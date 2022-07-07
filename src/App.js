184acbfb78b2fa45dda77103beda298c7087cf4f
HanningtoneM <hanningtonepompyy@gmail.com>
2022-07-07 15:23:38 +0300

    Add categories.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Categories from './components/Categories';
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
   </BrowserRouter>
 );

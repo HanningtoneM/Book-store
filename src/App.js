74c4674d6bfc90d559c02847fa9647ffa19e77ff
HanningtoneM <hanningtonepompyy@gmail.com>
2022-07-07 15:04:43 +0300

    Add Header.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+  </BrowserRouter>
+);
+
+export default App;

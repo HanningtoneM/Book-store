a519333911fe0654b0b8c604f78b5500ec1a5b17
HanningtoneM <hanningtonepompyy@gmail.com>
2022-07-13 17:08:04 +0300

    Add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 
 const Header = () => (
   <header>
-    <h1>Bookstore</h1>
-    <nav>
-      <Link to="/" className="nav-link">BOOKS</Link>
-      <Link to="/categories" className="nav-link">CATEGORIES</Link>
+    <nav className="my-nav">
+      <Link to="/" className="my-logo">BookStore CMS</Link>
+      <div className="link-container">
+        <Link className="my-link" to="/">BOOKS</Link>
+        <Link className="my-link" to="/categories">CATEGORIES</Link>
+      </div>
+      <Link to="/" className="my-user"><div className="user-container"><FaUser /></div></Link>
     </nav>
   </header>
 );

ae021d8f1c6127d1700eb99affb8f500ee62e361
HanningtoneM <hanningtonepompyy@gmail.com>
2022-07-13 11:23:48 +0300

    Add categories to categories.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,46 +1,107 @@
 import React, { useState } from 'react';
 import './Form.css';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/books';
+import { addBookToAPI } from '../redux/books/books';
+
+const categories = [
+  'Science',
+  'Fiction',
+  'Romance',
+  'Comical',
+  'Programming',
+];
 
 const Form = () => {
-  const [titleValue, setTitleValue] = useState();
-  const [authorValue, setAuthorValue] = useState();
-
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [author, setAuthor] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
   const dispatch = useDispatch();
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    dispatch(addBook({
-      title: titleValue,
-      author: authorValue,
-    }));
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const displayErrorMessage = (msg) => {
+    setErrorMsg(msg);
+    setTimeout(() => {
+      setErrorMsg('');
+    }, 2000);
+  };
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    if (title.trim().length === 0) {
+      setTitle('');
+      displayErrorMessage('Please input title');
+      return;
+    }
+    if (author.trim().length === 0) {
+      setAuthor('');
+      displayErrorMessage('Please input author');
+      return;
+    }
+    if (category === '') {
+      displayErrorMessage('Please select a Category');
+      return;
+    }
+    const newBook = {
+      item_id: Date.now(),
+      title,
+      author,
+      category,
+    };
+    dispatch(addBookToAPI(newBook));
+    setTitle('');
+    setAuthor('');
+    setCategory('');
   };
 
   return (
     <div className="form-container">
       <h2 className="form-title">ADD BOOK</h2>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={submitBookToStore}>
         <input
           className="form-book-title"
           type="text"
           required
           placeholder="Title"
-          value={titleValue}
-          onChange={(event) => setTitleValue(event.target.value)}
+          value={title}
+          onChange={onTitleChange}
         />
         <input
           className="form-book-author"
           type="text"
           required
           placeholder="Author"
-          value={authorValue}
-          onChange={(event) => setAuthorValue(event.target.value)}
+          value={author}
+          onChange={onAuthorChange}
         />
+        <div className="form-book-category">
+          <select className="select" default name="category" value={category} onChange={onCategoryChange}>
+            <option value="">
+              Category
+            </option>
+            {categories.sort().map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
         <button className="add-btn" type="submit">
           ADD BOOK
         </button>
       </form>
+      <p>{errorMsg}</p>
     </div>
   );
 };
